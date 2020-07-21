950fa8df68478208bbfc9811c31f78e355033ac0
idrak888 <playbox8g@gmail.com>
2020-07-21 20:19:13 +0800

    fixed endpoints

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tickerblue
-<img src="https://firebasestorage.googleapis.com/v0/b/tickeroid.appspot.com/o/logo.png?alt=media&token=9404e973-e329-4bb1-bba1-04d2a4f3d5e7" width="150"/>
+<img src="https://firebasestorage.googleapis.com/v0/b/tickeroid.appspot.com/o/Screenshot%202020-07-21%20at%202.48.49%20PM.png?alt=media&token=3c4aa4e5-76b7-43e1-ad4c-a6497b8c429b" width="150"/>
 <h3>Fundamental analysis made effortless | Web-scraping and ML</h3>
 <p>Technologies used:</p>
 <ul>
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.get("/tickerblue/fundamentals", (req, res) => {
+app.post("/tickerblue/fundamentals", (req, res) => {
 	const tickerSymbol = req.body.tickerSymbol;
 
 	extractFundamentals(`${tickerSymbol} stock news`).then(result => {
@@ -28,7 +28,7 @@ app.get("/tickerblue/fundamentals", (req, res) => {
 	});
 });
 
-app.get("/tickerblue/technicals", (req , res) => {
+app.post("/tickerblue/technicals", (req , res) => {
 	const tickerSymbol = req.body.tickerSymbol;
 	
 	extractTechnicals(tickerSymbol).then(result => {
